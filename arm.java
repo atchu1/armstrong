a6f29f823bf768137f3bd14aac7042949305ec87
atchu1 <atchayaraj18@gmail.com>
2019-02-20 14:48:10 +0530

    Create arm.java

---
+++ arm.java
@@ -0,0 +1,19 @@
+public class Armstrong {
+
+    public static void main(String[] args) {
+int num = 1634, originalNumber, rem, result = 0, n = 0;
+originalNumber = num;
+for (;originalNumber != 0; originalNumber /= 10, ++n);
+originalNumber = num;
+   for (;originalNumber != 0; originalNumber /= 10)
+        {
+            rem= originalNumber % 10;
+            result += Math.pow(rem, n);
+        }
+
+        if(result == num)
+            System.out.println("yes");
+        else
+            System.out.println("no");
+    }
+}
